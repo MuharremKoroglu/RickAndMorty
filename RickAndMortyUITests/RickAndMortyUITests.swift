7b702781b8cb8803d2b9ada7650e848e8c8a062d
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-08-30 13:57:50 +0300

    application completed

--- RickAndMortyUITests/RickAndMortyUITests.swift
+++ RickAndMortyUITests/RickAndMortyUITests.swift
@@ -18,13 +18,12 @@ final class RickAndMortyUITests: XCTestCase {
         app.launch()
         let collectionViewsQuery = app.collectionViews
         
-        // MARK: -Wait1
-        let wait1 = app.waitForExistence(timeout: timeout)
-        
-        XCTAssertTrue(wait1)
+        XCTAssertTrue(collectionViewsQuery.element.waitForExistence(timeout: timeout))
 
         // MARK: -Set up Location CollectionView Go Left
         let verticalCollectionViewGoLeft = collectionViewsQuery.containing(.other, identifier:"Vertical scroll bar, 11 pages").element
+        
+        XCTAssertTrue(verticalCollectionViewGoLeft.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(verticalCollectionViewGoLeft.exists)
 
@@ -36,18 +35,17 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Earth (Replacement Dimension) Location Button
         let locationButton1 = collectionViewsQuery.staticTexts["Earth (Replacement Dimension)"]
         
+        XCTAssertTrue(locationButton1.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(locationButton1.exists)
         
         locationButton1.tap()
         
-        // MARK: -Wait2
-        let wait2 = app.waitForExistence(timeout: timeout)
-        
-        XCTAssertTrue(wait2)
-        
         // MARK: -Set up Character CollectionView
         let horizontalCollectionView = collectionViewsQuery.containing(.other, identifier:"Horizontal scroll bar, 1 page").element
         
+        XCTAssertTrue(horizontalCollectionView.waitForExistence(timeout: timeout))
+
         XCTAssertTrue(horizontalCollectionView.exists)
         
         horizontalCollectionView.swipeUp()
@@ -58,6 +56,8 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character CollectionView Summer Smith Cell
         let characterCell = collectionViewsQuery.cells.otherElements.containing(.staticText, identifier:"Summer Smith").element
         
+        XCTAssertTrue(characterCell.waitForExistence(timeout: timeout))
+
         XCTAssertTrue(characterCell.exists)
 
         characterCell.tap()
@@ -67,6 +67,8 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character Episodes CollectionView
         let characterDetailEpisodeCollectionView = collectionViewsQuery.children(matching: .scrollView).element
         
+        XCTAssertTrue(characterDetailEpisodeCollectionView.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(characterDetailEpisodeCollectionView.exists)
         
         characterDetailEpisodeCollectionView.swipeLeft()
@@ -75,14 +77,11 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character Detail Episodes CollectionView Rixty Minutes Cell
         let characterEpisode = collectionViewsQuery.staticTexts["Rixty Minutes"]
         
+        XCTAssertTrue(characterEpisode.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(characterEpisode.exists)
 
         characterEpisode.tap()
-        
-        // MARK: -Wait3
-        let wait3 = app.waitForExistence(timeout: timeout)
-        
-        XCTAssertTrue(wait3)
 
         horizontalCollectionView.swipeUp(velocity: 3000)
         horizontalCollectionView.swipeUp(velocity: 3000)
@@ -91,6 +90,8 @@ final class RickAndMortyUITests: XCTestCase {
         
         // MARK: -Set up Character Detail Episodes CollectionView Rixty Minutes Cell
         let episodeDetailNavigationBar = app.navigationBars["Episode"].buttons["SUMMER SMITH"]
+        
+        XCTAssertTrue(episodeDetailNavigationBar.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(episodeDetailNavigationBar.exists)
         
@@ -102,18 +103,17 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character Detail Screen Navigation Bar
         let navigationBar = app.navigationBars["SUMMER SMITH"].buttons["Characters"]
         
+        XCTAssertTrue(navigationBar.waitForExistence(timeout: timeout))
+
         XCTAssertTrue(navigationBar.exists)
 
         navigationBar.tap()
-        
-        // MARK: -Wait4
-        let wait4 = app.waitForExistence(timeout: timeout)
-        
-        XCTAssertTrue(wait4)
 
         // MARK: -Set up Location CollectionView Go Right
         let verticalCollectionViewGoRight = collectionViewsQuery.containing(.other, identifier:"Vertical scroll bar, 20 pages").element
         
+        XCTAssertTrue(verticalCollectionViewGoRight.waitForExistence(timeout: timeout))
+
         XCTAssertTrue(verticalCollectionViewGoRight.exists)
 
         verticalCollectionViewGoRight.swipeRight(velocity: 5000)
@@ -122,6 +122,8 @@ final class RickAndMortyUITests: XCTestCase {
         
         // MARK: -Set up Earth (C-137) Location Button
         let locationButton2 = collectionViewsQuery.staticTexts["Earth (C-137)"]
+        
+        XCTAssertTrue(locationButton2.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(locationButton2.exists)
 
@@ -134,39 +136,41 @@ final class RickAndMortyUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
         
-        let timeout: TimeInterval = 2
-        
+        let timeout: TimeInterval = 5
+    
         // MARK: -Set up TabBar
-        let tabBar = XCUIApplication().tabBars["Tab Bar"]
+        let tabBar = app.tabBars["Tab Bar"]
+        
+        XCTAssertTrue(tabBar.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(tabBar.exists)
-
+        
         // MARK: -Set up EpisodeTab
         let episodeTab = tabBar.buttons["Episodes"]
         
+        XCTAssertTrue(episodeTab.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(episodeTab.exists)
-
+        
         episodeTab.tap()
         
         // MARK: -Set up TableView
         let tableView = app.tables.element
         
+        XCTAssertTrue(tableView.waitForExistence(timeout: timeout))
+
         XCTAssertTrue(tableView.exists)
 
         tableView.swipeUp(velocity: 5000)
         tableView.swipeUp(velocity: 5000)
         tableView.swipeUp(velocity: 5000)
-        
-        // MARK: -Wait1
-        let wait1 = app.wait(for: .runningForeground, timeout: timeout)
-        
-        XCTAssertTrue(wait1)
-
         tableView.swipeUp(velocity: 5000)
         tableView.swipeUp(velocity: 5000)
 
         // MARK: -Set up TableView Forgetting Sarick Mortshall Cell
         let cell = app.tables.staticTexts["Forgetting Sarick Mortshall"]
+        
+        XCTAssertTrue(cell.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(cell.exists)
 
@@ -175,6 +179,8 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character CollectionView in Episode Detail Screen
         let horizontalScrollBar1PageCollectionView = app.collectionViews.containing(.other, identifier:"Horizontal scroll bar, 1 page").element
         
+        XCTAssertTrue(horizontalScrollBar1PageCollectionView.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(horizontalScrollBar1PageCollectionView.exists)
 
         horizontalScrollBar1PageCollectionView.swipeUp()
@@ -182,6 +188,8 @@ final class RickAndMortyUITests: XCTestCase {
         
         // MARK: -Set up CollectionView Harold's Wife Cell
         let characterCell = app.collectionViews.cells.otherElements.containing(.staticText, identifier:"Harold's Wife").element
+        
+        XCTAssertTrue(characterCell.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(characterCell.exists)
 
@@ -193,20 +201,18 @@ final class RickAndMortyUITests: XCTestCase {
         // MARK: -Set up Character Detail Screen Navigation Bar
         let characterNavigationBar = app.navigationBars["HAROLD'S WIFE"].buttons["Episode"]
         
+        XCTAssertTrue(characterNavigationBar.waitForExistence(timeout: timeout))
+        
         XCTAssertTrue(characterNavigationBar.exists)
 
         characterNavigationBar.tap()
 
         horizontalScrollBar1PageCollectionView.swipeDown(velocity: 2000)
 
-        // MARK: -Wait2
-        let wait2 = app.wait(for: .runningForeground, timeout: timeout)
-        
-        XCTAssertTrue(wait2)
-
-
         // MARK: -Set up Episode Detail Screen Navigation Bar
         let navigationBar = app.navigationBars["Episode"].buttons["Episodes"]
+        
+        XCTAssertTrue(navigationBar.waitForExistence(timeout: timeout))
         
         XCTAssertTrue(navigationBar.exists)
 
